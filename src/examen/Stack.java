24ca9d030c184da65547c7227d1e386acf5e221c
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2019-03-12 10:46:07 +0100

    a

--- src/examen/Stack.java
+++ src/examen/Stack.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 
 /**
  * Solution of exercise 1
+ * 
  * @author joaalsai
  *
  */
@@ -24,33 +25,26 @@ public class Stack<E> implements Serializable {
 
 	public void push(E q) {
 		Nodo<E> nodo = new Nodo<E>(q);
-		Nodo<E> aux = peak;
+
 		if (peak == null)
 			peak = nodo;
 		else {
-			while (aux.getNext() != null) {
-				aux = aux.getNext();
-			}
-			nodo.setPrevious(aux);
-			aux.setNext(nodo);
+			nodo.setNext(peak);
+			peak.setPrevious(nodo);
 			peak = nodo;
 		}
 	}
 
 	public E pop() {
-		Nodo<E> aux = peak;
 
-		if (peak == null)
-			return null;
-		else if (peak.getPrevious() == null) {
-			aux = peak;
-			peak = null;
-			return aux.getElement();
-		} else {
-			peak = peak.getPrevious();
-			peak.setNext(null);
-			return aux.getElement();
+		E aux = null;
+		if (peak != null) {
+			aux = peak.getElement();
+			peak = peak.getNext();
+			if (peak != null)
+				peak.setPrevious(null);
 		}
+		return aux;
 
 	}
 
@@ -59,7 +53,7 @@ public class Stack<E> implements Serializable {
 		String salida = "";
 		while (aux != null) {
 			salida = salida + aux.toString();
-			aux = aux.getPrevious();
+			aux = aux.getNext();
 		}
 		return salida;
 	}
